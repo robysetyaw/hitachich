f15daa9f5737191eac76bc696957447a0f180160
900158 <900158@WCH-900158-1>
2025-09-09 10:28:44 +0700

    add readme.md and change print func

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
-import "hi-ch/hitachich"
+import (
+	"fmt"
+	"hi-ch/hitachich"
+)
 
 func main() {
 	minpairGot := hitachich.MinPair([]int{1, 4, 3, 2})
 	palindromeGot := hitachich.Palindrome("aaa")
 	numberCombineGot := hitachich.NumberCombine("23")
 
-	println("minpair input = [1, 4, 3, 2] got = ", minpairGot)
-	println("palindrome input = aaa got = ", palindromeGot)
-	println("numberCombine input = 23 got = ", numberCombineGot)
+	fmt.Println("minpair input = [1, 4, 3, 2], got =", minpairGot)
+	fmt.Println("palindrome input = aaa, got =", palindromeGot)
+	fmt.Println("numberCombine input = 23, got =", numberCombineGot)
 }
---
+++ readme.md
@@ -0,0 +1,20 @@
+# HOW TO RUN CODE
+
+## Clone Repository
+
+> git clone https://github.com/robysetyaw/hitachich
+
+## Get In
+
+> cd hitachich
+
+## Go Mod Tidy
+
+> go mod tidy
+
+## Run the project
+
+> go run main.go
+
+## Result
+![alt text](image.png)
